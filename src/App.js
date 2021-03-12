16eb709670b9a0991e5bfac1b35280052c8d5e0a
sickshit <sickshit>
2021-03-12 15:57:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Table from './start';
+import { Link } from 'react-router-dom';
+import create from './create';
+
+<NavLink activeClassName="active" to="/about">About</NavLink>
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Dogbook</h1>
+        <br/>
+        <h2>Users</h2>
+        <Table />
+        <Link to="/create.js">Add new dog</Link>
+        <Route
+          Path="/create.js"
+          component={HomeComponent}
+          exact
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
